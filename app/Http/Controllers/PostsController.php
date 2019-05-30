020a5718a0f0339a50a42c101943484347cab6af
Alex Mozheiko <aliaksei.mazheika@itechart-group.com>
2019-05-30 15:42:23 +0300

    Auth require

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+
+	public function __construct() {
+		$this->middleware('auth');
+	}
+
+
+	//
 	public function create(){
 
 		return view('posts.create');

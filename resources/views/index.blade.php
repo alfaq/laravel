236bb1adb62f05e1720df79e629664ac3ef4dd09
Alex Mozheiko <aliaksei.mazheika@itechart-group.com>
2019-06-05 14:28:13 +0300

    Template for home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,123 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <i class="far fa-list-alt"></i> Category
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                </div>
+
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <i class="far fa-address-card"></i> New users
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                </div>
+
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <i class="far fa-comments"></i> New comments
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-8">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h2><a href="#">Post title</a></h2>
+                        <div class="mb-2" style='width: 100%; height: 200px; background: url("/storage/uploads/6eA6qqJH54L2ZXGXAYllnbEbKW2hX5xgINMwMvqW.png"); background-repeat: no-repeat;
+                            background-size: cover;
+                            background-position: 50%;'>
+                        </div>
+                        <div class="card-text mb-2">
+                            <a href="#" style="color: gray; font-size: 12px;"><i class="far fa-list-alt"></i> Category</a>
+                            <span class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-clock"></i> 5-06-2019 3:44am</span>
+                            <span class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-eye"></i> 32 </span>
+                            <span class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-comments"></i> 10 </span>
+                            <a href="#" class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-user"></i> admin</a>
+                        </div>
+                        <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis fugit natus numquam quos rem. Eos et ex excepturi impedit incidunt labore libero nisi placeat! Accusamus consectetur exercitationem quisquam repudiandae ullam!</div>
+                        <div class="text-right"><a href="#"><i class="fas fa-angle-right"></i> Read more...</a></div>
+                        <hr>
+                    </div>
+                </div>
+
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h2><a href="#">Post title</a></h2>
+                        <div class="mb-2" style='width: 100%; height: 200px; background: url("/storage/uploads/6eA6qqJH54L2ZXGXAYllnbEbKW2hX5xgINMwMvqW.png"); background-repeat: no-repeat;
+                            background-size: cover;
+                            background-position: 50%;'>
+                        </div>
+                        <div class="card-text mb-2">
+                            <a href="#" style="color: gray; font-size: 12px;"><i class="far fa-list-alt"></i> Category</a>
+                            <span class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-clock"></i> 5-06-2019 3:44am</span>
+                            <span class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-eye"></i> 32 </span>
+                            <span class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-comments"></i> 10 </span>
+                            <a href="#" class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-user"></i> admin</a>
+                        </div>
+                        <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis fugit natus numquam quos rem. Eos et ex excepturi impedit incidunt labore libero nisi placeat! Accusamus consectetur exercitationem quisquam repudiandae ullam!</div>
+                        <div class="text-right"><a href="#"><i class="fas fa-angle-right"></i> Read more...</a></div>
+                        <hr>
+                    </div>
+                </div>
+
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h2><a href="#">Post title</a></h2>
+                        <div class="mb-2" style='width: 100%; height: 200px; background: url("/storage/uploads/6eA6qqJH54L2ZXGXAYllnbEbKW2hX5xgINMwMvqW.png"); background-repeat: no-repeat;
+                            background-size: cover;
+                            background-position: 50%;'>
+                        </div>
+                        <div class="card-text mb-2">
+                            <a href="#" style="color: gray; font-size: 12px;"><i class="far fa-list-alt"></i> Category</a>
+                            <span class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-clock"></i> 5-06-2019 3:44am</span>
+                            <span class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-eye"></i> 32 </span>
+                            <span class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-comments"></i> 10 </span>
+                            <a href="#" class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-user"></i> admin</a>
+                        </div>
+                        <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis fugit natus numquam quos rem. Eos et ex excepturi impedit incidunt labore libero nisi placeat! Accusamus consectetur exercitationem quisquam repudiandae ullam!</div>
+                        <div class="text-right"><a href="#"><i class="fas fa-angle-right"></i> Read more...</a></div>
+                        <hr>
+                    </div>
+                </div>
+
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h2><a href="#">Post title</a></h2>
+                        <div class="mb-2" style='width: 100%; height: 200px; background: url("/storage/uploads/6eA6qqJH54L2ZXGXAYllnbEbKW2hX5xgINMwMvqW.png"); background-repeat: no-repeat;
+                            background-size: cover;
+                            background-position: 50%;'>
+                        </div>
+                        <div class="card-text mb-2">
+                            <a href="#" style="color: gray; font-size: 12px;"><i class="far fa-list-alt"></i> Category</a>
+                            <span class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-clock"></i> 5-06-2019 3:44am</span>
+                            <span class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-eye"></i> 32 </span>
+                            <span class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-comments"></i> 10 </span>
+                            <a href="#" class="ml-2" style="color: gray; font-size: 12px;"><i class="far fa-user"></i> admin</a>
+                        </div>
+                        <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis fugit natus numquam quos rem. Eos et ex excepturi impedit incidunt labore libero nisi placeat! Accusamus consectetur exercitationem quisquam repudiandae ullam!</div>
+                        <div class="text-right"><a href="#"><i class="fas fa-angle-right"></i> Read more...</a></div>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

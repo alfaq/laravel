e4b8fddaad9fc98dc20b9573bd9c761b7eff62e1
Alex Mozheiko <aliaksei.mazheika@itechart-group.com>
2019-06-26 16:10:53 +0300

    Dashboard link for admin role

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,9 +25,11 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">Blogs</a>
-                @if (Route::has('register'))
+                @auth
+                @if (Auth::user()->role_id == 1)
                     <a href="/dashboard" target="_blank">(Visit dashboard)</a>
                 @endif
+                @endauth
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>

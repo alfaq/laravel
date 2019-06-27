3a4df60e026ff3c1989e6e13da2cde4178aa6b53
Alex Mozheiko <aliaksei.mazheika@itechart-group.com>
2019-06-27 17:34:01 +0300

    Cache

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
 	public function index(){
-	    $posts = Post::orderBy('created_at', 'desc')->paginate(5);
+		$minutes = 10;
+		$posts = Cache::remember('home_page_posts', $minutes, function () {
+			return Post::orderBy('created_at', 'desc')->paginate(5);
+		});
 
     	return view('index', compact('posts'));
     }

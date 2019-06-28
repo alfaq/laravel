2e78beb432ee1d18567e1a6a5ecc7f5bb2b6fa39
Alex Mozheiko <aliaksei.mazheika@itechart-group.com>
2019-06-28 15:20:20 +0300

    Add Telescope

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,8 +8,7 @@ use Illuminate\Support\Facades\Cache;
 class HomeController extends Controller
 {
 	public function index(){
-		$minutes = 10;
-		$posts = Cache::remember('home_page_posts', $minutes, function () {
+		$posts = Cache::remember('home_page_posts', now()->addSeconds(60), function () {
 			return Post::orderBy('created_at', 'desc')->paginate(5);
 		});
 

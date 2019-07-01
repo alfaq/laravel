290ca360e4f63530dc528e3d2f2b2ed4074d8b20
Alex Mozheiko <aliaksei.mazheika@itechart-group.com>
2019-07-01 13:13:28 +0300

    Files

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
 
 class HomeController extends Controller
 {
@@ -11,6 +12,18 @@ class HomeController extends Controller
 		$posts = Cache::remember('home_page_posts', now()->addSeconds(60), function () {
 			return Post::orderBy('created_at', 'desc')->paginate(5);
 		});
+
+		//работа с файлами
+		//Storage::disk('local')->put('public/file.txt', 'Contents');
+		//http://laravel.su/docs/5.4/filesystem
+		Storage::put('public/file.txt', 'Contents');
+		if($exists = Storage::exists('public/file.txt')) {
+			$contents = Storage::get( 'public/file.txt' );
+			//dd( $contents );
+
+			$url = Storage::url('public/file.txt');
+			dd( $url );
+		}
 
     	return view('index', compact('posts'));
     }
